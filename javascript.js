9505632c98893a949b3f23d2a462c9fb4049635b
kelvin-martey <150729637+kelvin-martey@users.noreply.github.com>
2023-11-27 23:35:59 +0000

    js

---
+++ javascript.js
@@ -0,0 +1,32 @@
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  ...
+});
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
